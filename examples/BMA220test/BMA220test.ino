ce21288c9f6229fd8ed61ca9b833cb18412b6228
scbrac <scbrac@users.noreply.github.com>
2016-07-24 12:41:51 +0200

    Create BMA220test.ino

---
+++ examples/BMA220test/BMA220test.ino
@@ -0,0 +1,22 @@
+#include <BMA220.h>
+
+BMA220 bma;
+
+void setup() {
+    Serial.begin(9600);
+    if (!bma.begin()) {
+        Serial.println(F("No valid BMA220 sensor found, check wiring"));
+        while (true):  // stop here, no reason to go on...
+            ;
+    }
+}
+
+void loop() {
+    Serial.print("x-axis: ");
+    Serial.println(bma.readAcceleration(XAXIS));
+    Serial.print("y-axis: ");
+    Serial.println(bma.readAcceleration(YAXIS));
+    Serial.print("z-axis: ");
+    Serial.println(bma.readAcceleration(ZAXIS));
+    delay(1000);
+}
